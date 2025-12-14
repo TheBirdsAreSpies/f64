4ba0df19dcce3bd62b451e11d2e9c8933abc5f95
Fabian Bauer <shore@skreamyourlungsout.com>
2025-12-14 21:35:34 +0100

    fix(auth-schema): setting no password is returning cryptic error message

--- shared/schemas/auth.schema.ts
+++ shared/schemas/auth.schema.ts
@@ -3,12 +3,11 @@ import * as z from "zod"
 
 export function createStandardLoginSchema(t: Composer["t"]): z.ZodSchema<StandardLoginInput> {
   return z.object({
-    email: z
-      .string()
-      .email(t("login_validation_invalid_email")),
-    password: z
-      .string()
-      .min(4, t("login_validation_min_length", { count: 4 })),
+    email: z.email(t("login_validation_invalid_email")),
+    password: z.preprocess(
+      val => val || "",
+      z.string().min(4, t("login_validation_min_length", { count: 4 })),
+    ),
   })
 }
 
