1d058ec9d21341f91c38eaa73c5bedcfbce1d6e6
Fabian Bauer <shore@skreamyourlungsout.com>
2025-12-15 00:21:22 +0100

    feat(permission): add permissions

--- server/types/permissions.ts
+++ server/types/permissions.ts
@@ -8,4 +8,35 @@ export enum PostPermission {
   EditAny = "post_EditAny",
 }
 
-export type Permission = UserPermission | PostPermission
+export enum PhotoPermission {
+  Upload = "photo_Upload",
+  Edit = "photo_Edit",
+  EditAny = "photo_EditAny",
+  Delete = "photo_Delete",
+  DeleteAny = "photo_DeleteAny",
+  ViewPrivate = "photo_ViewPrivate",
+}
+
+export enum AlbumPermission {
+  Create = "album_Create",
+  Edit = "album_Edit",
+  EditAny = "album_EditAny",
+  Delete = "album_Delete",
+  DeleteAny = "album_DeleteAny",
+  ViewPrivate = "album_ViewPrivate",
+}
+
+export enum TagPermission {
+  Create = "tag_Create",
+  Edit = "tag_Edit",
+  Delete = "tag_Delete",
+}
+
+export enum ThemePermission {
+  Create = "theme_Create",
+  Edit = "theme_Edit",
+  Delete = "theme_Delete",
+  Activate = "theme_Activate",
+}
+
+export type Permission = UserPermission | PostPermission | PhotoPermission | AlbumPermission | TagPermission | ThemePermission
